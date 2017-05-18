cc4097d429f792669fdaeabb17ac26dd19546b8d
root <root@vultr.guest>
2017-05-18 08:41:56 +0000

    acc0.98

--- cnntrain.py
+++ cnntrain.py
@@ -5,11 +5,6 @@ from tflearn.layers.estimator import regression
 import numpy as np
 import cv2
 
-f=open('../data/d.txt','r')
-f=f.read()
-f=f.split('\n')
-f=f[0:len(f)-1]
-totallen=len(f)
 
 if 0:
     Y=np.empty((totallen,10))
@@ -31,19 +26,19 @@ else:
     X = np.load(namelist[0], mmap_mode='r+')
     Y=np.load("data32/Y.npy")
 
+totallen=len(Y)
 
-
-trainsize=int(0.95*totallen)
+trainsize=int(0.8*totallen)
 
 convnet = input_data(shape=[None, 32, 32, 1], name='input')
 #layer1
 convnet = conv_2d(convnet, 32, 2, activation='relu')
 convnet = max_pool_2d(convnet, 2)
 #layer2
-convnet = conv_2d(convnet, 64, 2, activation='relu')
+convnet = conv_2d(convnet, 32, 2, activation='relu')
 convnet = max_pool_2d(convnet, 2)
 #layer3
-convnet = fully_connected(convnet, 1024, activation='relu')
+convnet = fully_connected(convnet, 128, activation='relu')
 convnet = dropout(convnet, 0.8)
 #layer4
 convnet = fully_connected(convnet, 10, activation='softmax')
@@ -52,7 +47,7 @@ convnet = regression(convnet, optimizer='adam', learning_rate=0.0001, loss='cate
 model = tflearn.DNN(convnet,tensorboard_dir='log')
 
 model.load('model/symb.model')
-model.fit({'input': X[0:trainsize]}, {'targets': Y[0:trainsize]}, n_epoch=20, validation_set=({'input': X[trainsize:totallen]}, {'targets': Y[trainsize:totallen]}),
+model.fit({'input': X[0:trainsize]}, {'targets': Y[0:trainsize]}, n_epoch=5, validation_set=({'input': X[trainsize:totallen]}, {'targets': Y[trainsize:totallen]}),
     snapshot_step=500, show_metric=True, run_id='symb')
 
 model.save('model/symb.model')
